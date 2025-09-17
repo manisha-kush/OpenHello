24e002a40bc9d3829b76fed33c776ced1aa6666f
Manisha Kushwaha <manishakushwaha-cmpn@atharvacoe.ac.in>
2025-09-17 13:30:44 +0530

    Update contributorsData.jsx

--- client/src/data/contributorsData.jsx
+++ client/src/data/contributorsData.jsx
@@ -1,23 +1,9 @@
 export const contributorsData = [
-  {
-    name: "Siddesh Navthale",
-    photoUrl: "https://avatars.githubusercontent.com/u/111569738?v=4",
-    githubUrl: "https://github.com/sidxhdev",
-  },
-  {
-    name: "Sahil Rewale",
-    photoUrl: "https://avatars.githubusercontent.com/u/223218760?s=400&u=472b5d6906f12cd670485c037f1790d93cfe29e7&v=4",
-    githubUrl: "https://github.com/sahilrewale-09",
-  },
-  {
-    name: "Manali",
-    photoUrl: "https://avatars.githubusercontent.com/u/180926010?v=4",
-    githubUrl: "https://github.com/manali-0715",
-  },
-  {
-    name: "Vamdeet",
-    photoUrl: "https://avatars.githubusercontent.com/u/180926010?v=4",
-    githubUrl: "https://github.com/Vandeetshah17",
+
+   {
+    name: "Manisha kushwaha",
+    photoUrl: "https://avatars.githubusercontent.com/u/220671574?s=400&u=2a1853c3f9a340d5752a299177c41e42d0e6e7c8&v=4",
+    githubUrl: "https://github.com/manisha-kush",
   },
   
  
